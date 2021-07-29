4d95eaa647c89a36dae414c38e59c7b4ccd3f0bd
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-07-29 23:13:47 +0800

    Initial Commit

---
+++ Heart/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Heart
+//
+//  Created by 邱宏宇 on 7/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Heart/HeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartApp.swift
+//  Heart
+//
+//  Created by 邱宏宇 on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct HeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

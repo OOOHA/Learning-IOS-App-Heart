9d2662b8754de314033b87a7142c3d1b76c3861b
OOOHA <45551120+OOOHA@users.noreply.github.com>
2021-12-29 22:49:43 +0800

    first

--- Heart/ContentView.swift
+++ Heart/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var colorChange = false
+    @State private var sizechange = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            Image(systemName: "heart.fill")
+                .font(.system(size: 200))
+                .foregroundColor(colorChange ? Color("LightRed") : Color("Red"))
+                .scaleEffect(sizechange ? 1.5 : 1)
+                .animation(.default)
+                .onTapGesture {
+                    self.colorChange.toggle()
+                }
+                .onLongPressGesture {
+                    self.sizechange.toggle()
+                }
+            
+        }
+        
     }
 }
 
